c339f4f4d15d53037f3f746bb72515636cdc63cd
Gianna Mastro <giannamastro02@gmail.com>
2023-01-14 21:39:02 -0500

    commit tree 286adb8a1e6eae5c017d9f92105128dac9b8ff19

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+    return cats;
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+    return cats;
+}
+function destructivelyRemoveLastCat() {
+    cats.pop(cats);
+    return cats;
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift(cats);
+    return cats;
+}
+
+function appendCat(name) {
+    const copyOfCats = [...cats, "Broom"];
+    return copyOfCats;
+}
+function prependCat(name) {
+    const copyOfCats = ["Arnold", ...cats];
+    return copyOfCats;
+}
+function removeLastCat() {
+    const copyOfCats = [...cats];
+    copyOfCats.pop();
+    return copyOfCats;
+}
+function removeFirstCat() {
+    const copyOfCats = [...cats];
+    copyOfCats.shift();
+    return copyOfCats;
+}
